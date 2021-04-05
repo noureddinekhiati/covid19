6bfa3a2ee904f67682881180938ab0c3903b6770
noureddinekhiati <noureddinekhiati14@gmail.Com>
2021-04-05 16:36:39 +0200

    Fixing only css,boostrap

---
+++ src/main/java/com/covid19/covid19Controller.java
@@ -0,0 +1,27 @@
+package com.covid19;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Date;
+
+@Controller
+public class covid19Controller {
+    private String appMode;
+    @Autowired
+    public covid19Controller(Environment environment){
+        appMode = environment.getProperty("app-mode");
+    }
+
+    @RequestMapping("/")
+    public String index(Model model){
+        model.addAttribute("datetime", new Date());
+        model.addAttribute("username", "Ömerrrr");
+        model.addAttribute("mode", appMode);
+
+        return "index";
+    }
+}
