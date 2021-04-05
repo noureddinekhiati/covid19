7a60bc7f320fadbb2f2e89661962fab6772fd8e2
noureddinekhiati <noureddinekhiati14@gmail.Com>
2021-04-05 12:43:43 +0200

    Initial commit

---
+++ COVID2019/src/test/java/com/example/covid2019/demo/Covid2019ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.covid2019.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid2019ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
